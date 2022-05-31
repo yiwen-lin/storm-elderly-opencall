9f8ac6a39d303457fb6a9777994d7e40e2f8151b
YiWen Lin <yiwenlin@cw.com.tw>
2022-05-31 14:39:20 +0800

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -590,3 +590,47 @@
           }
         });
       })
+
+
+//lightbox
+$('body').append(`
+    <div class="lightbox">
+      <a href="#lightbox" class="lightbox-close lightbox-toggle">╳</a>
+      <div class="lightbox-container">
+        <div class="row">
+          <div class="col-sm-12 lightbox-column">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+`);
+
+$('.lightbox-toggle').on('click', (event) => {
+  event.preventDefault();
+  $('.lightbox').fadeToggle('fast');
+  
+  let context = $(event.currentTarget).attr('data-lightbox-type');
+  let content = $(event.currentTarget).attr('data-lightbox-content');
+  console.log(event);
+  if (context == 'video') {
+    $('.lightbox-column').append(`
+        <div class="lightbox-video">
+        <iframe src="${content}" frameborder="0" allowfullscreen> </iframe>
+        </div>
+    `);
+  } else if (context == 'image') {
+    $('.lightbox-column').append(`
+        <img src="${content}" class="img-" frameborder="0" allowfullscreen>
+    `);
+  }
+});
+
+$('.lightbox-close').on('click', (event) => {
+  event.preventDefault();
+  $('.lightbox-column > *').remove();
+});
+
+
+
+
